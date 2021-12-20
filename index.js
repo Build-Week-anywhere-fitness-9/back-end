ddfc4ddc87c1d0659c178e9c39179e609f962f50
mark-escosura <escosuramarkse@gmail.com>
2021-12-20 10:36:14 -0800

    Project Setup

---
+++ index.js
@@ -0,0 +1,9 @@
+require("dotenv").config()
+const server = require("./api/server.js")
+
+const { PORT } = require("./config/secrets.js")
+
+server.listen(PORT, () => {
+  console.log(`..server listening on PORT ${PORT}..`)
+})
+
