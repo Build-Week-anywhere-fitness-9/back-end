ee12b07db521c12926e0f58dd0f95aae9e8e9dba
mark-escosura <escosuramarkse@gmail.com>
2021-12-21 21:58:38 -0800

    make it better

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,23 +1,59 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 
+const { tokenBuilder } = require("./auth-helpers.js");
 const { BCRYPT_ROUNDS } = require("../../config/secrets.js");
 const User = require("../users/users-model.js");
+const {
+  checkUsernameFree,
+  validateEmail,
+  validateBody,
+  validateUsername,
+} = require("./auth-middlewares.js");
 
 // [POST] register // creates a user into the database
-router.post("/register", async (req, res, next) => {
-  const { username, email, password, role_id } = req.body;
-  const hash = bcrypt.hashSync(password, BCRYPT_ROUNDS);
-  const user = await User.create({
-    username: username,
-    email: email,
-    password: hash,
-    role_id: role_id,
-  });
-  try {
-    res.status(201).json(user);
-  } catch (err) {
-    next(err);
+router.post(
+  "/register",
+  validateBody,
+  checkUsernameFree,
+  validateEmail,
+  async (req, res, next) => {
+    const { username, email, password, role_id } = req.body;
+
+    const hash = bcrypt.hashSync(password, BCRYPT_ROUNDS);
+
+    const newUser = {
+      username: username,
+      email: email,
+      password: hash,
+      role_id: role_id,
+    };
+
+    try {
+      const user = await User.create(newUser);
+      res.status(201).json(user);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+// [POST] login // logs in the user into the database
+router.post("/login", validateBody, validateUsername, (req, res, next) => {
+  const { password } = req.body;
+  if (bcrypt.compareSync(password, req.user.password)) {
+    const token = tokenBuilder(req.user);
+    res.json({
+      status: 200,
+      message: `welcome, ${req.user.username}`,
+      user_id: req.user.user_id,
+      token: token,
+    });
+  } else {
+    next({
+      status: 401,
+      message: "invalid credentials",
+    });
   }
 });
 
--- config/secrets.js
+++ config/secrets.js
@@ -2,4 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 9000,
   NODE_ENV: process.env.NODE_ENV || "development",
   BCRYPT_ROUNDS: process.env.BCRYPT_ROUNDS || 8,
+  JWT_SECRET: process.env.JWT_SECRET || "fitness"
 };
